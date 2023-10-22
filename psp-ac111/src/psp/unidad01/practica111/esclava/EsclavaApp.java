2cc4aa6587fdf1417dae436eca5bcb004df061a6
Daniel Galo <a_daniel.galo.vega@iespablopicasso.es>
2023-10-22 17:19:44 +0200

    Añado funcion de desviacion tipica a esclavaApp

--- psp-ac111/src/psp/unidad01/practica111/esclava/EsclavaApp.java
+++ psp-ac111/src/psp/unidad01/practica111/esclava/EsclavaApp.java
@@ -3,50 +3,93 @@ package psp.unidad01.practica111.esclava;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Programa que calcula la desviación típica de una serie de números: a cada
+ * entrada se le resta la media total de la lista (la cual es la primera entrada
+ * que se le pasa.). El resultado de esta resta elevarlo al cuadrado y los
+ * resultados de elevarlo al cuadrado se van sumando. La suma de los cuadrados
+ * es lo que devuelve el programa. Por ejemplo la media es 6 y las entradas son
+ * 5, 8, 9 -> ((5 - 6)^2) + ((8 - 6)^2) + ((9 - 6)^2) = número a devolver.
+ */
 public class EsclavaApp {
 
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+
 		// Buffer para leer la entrada
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		// Calcular la desviación: a cada entrada de la lista se le resta la media total
-		// de la lista (la cual es la primera entrada que se le pasa.).
-		// Al resultado de esta resta elevarlo al cuadrado y a los resultados de
-		// elevarlo al cuadrado se van sumando. La suma de los cuadrados es lo que
-		// devuelve el programa
-		// p ej la media es 6 y las entradas son 5, 8, 9
-		// 5 - 6 = -1^2
-		// 8 - 6 = 2^2
-		// 9 - 6 = 3^2
-		// ........ num a devolver
+
 		try {
-//
-//			String media = reader.readLine();
 
-			String linea;
+			// Conseguir la media, que es la primera entrada
+			double media = Double.parseDouble(reader.readLine());
 
-//			List<Double> nums = new ArrayList<>();
-//			boolean primeraLinea = true;
-//			while ((linea = reader.readLine()) != null) {
-//
-//				if (!primeraLinea) {
-//					nums.add(Double.parseDouble(linea));
-//				} else {
-//					primeraLinea = false;
-//				}
-//
-//			}
+			// Conseguir lista con los números de la entrada
+			List<Double> nums = getListaNumeros(reader);
 
-			while ((linea = reader.readLine()) != null) {
+			// Conseguir desviación, imprimirla
+			double desviacionTipica = getDesviacionTipica(media, nums);
+			System.out.println(desviacionTipica);
 
-				System.out.println(linea);
-			}
+			System.out.println(0);
 
 		} catch (IOException e) {
 
+			System.out.println(-1);
 			e.printStackTrace();
 
 		}
+	}
+
+	/**
+	 * Construye una lista con los números de la entrada estándar
+	 * 
+	 * @param reader
+	 * @return
+	 * @throws IOException
+	 */
+	private static List<Double> getListaNumeros(BufferedReader reader) throws IOException {
+
+		String linea;
+
+		List<Double> nums = new ArrayList<>();
+
+		while ((linea = reader.readLine()) != null) {
+
+			if (!linea.isBlank()) {
+				nums.add(Double.parseDouble(linea));
+			} else {
+				break;
+			}
+
+		}
+		return nums;
+	}
+
+	/**
+	 * Calcula la desviación típica de una lista
+	 * 
+	 * @param media
+	 * @param nums
+	 * @return
+	 */
+	private static double getDesviacionTipica(double media, List<Double> nums) {
+		double sumaTotal = 0;
+		double resta = 0;
+		double cuadradoResta = 0;
+		for (double numero : nums) {
+			resta = numero - media;
+			cuadradoResta = resta * resta;
+			sumaTotal += cuadradoResta;
+		}
+		return sumaTotal;
 	}
 
 }
